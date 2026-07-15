e28f43e8f3c8803489189c5c3e7a5518fd97c470
맹주영 <158379611+MaengJuyoung@users.noreply.github.com>
2026-07-15 16:34:01 +0900

    Update wedding.ts

--- src/api/wedding.ts
+++ src/api/wedding.ts
@@ -1,3 +1,6 @@
-export function getWedding() {
-  return fetch('http://localhost:8888/wedding')
+const WEDDING_DATA_URL =
+  'https://raw.githubusercontent.com/MaengJuyoung/weddinginvitation/main/db.json'
+
+export function getWedding(): Promise<Response> {
+  return fetch(WEDDING_DATA_URL)
 }
